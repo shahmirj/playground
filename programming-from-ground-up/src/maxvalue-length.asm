d5beaf50566f04bee43f4b972957017314b63925
Shahmir <shahmirj@gmail.com>
2014-02-13 23:41:58 +0000

    Check maxvalue using the length
    Added a `data_items_length` variable to a new assembly program
    `maxvalue-length.asm`, this allows user to define a size for the
    `data_items` list so we don't have to use zero as a stop.

---
+++ programming-from-ground-up/src/maxvalue-length.asm
@@ -0,0 +1,36 @@
+# Calculate the max value of the data_items
+#
+# To run:
+#     make bin/maxvalue
+#     bin/maxvalue
+#     echo $?
+#
+# The value returned should be 243
+
+.section .data
+data_items:
+ .long 12,45,3,23,4,5,4,3,234,5,45,43,3,243,123,4,23,4,0
+data_items_length:
+ .long 8
+
+.section .text
+
+.globl _start
+_start:
+    movl $0, %edi
+    movl data_items(,%edi,4), %eax
+    movl %eax, %ebx
+
+loop:
+    cmp data_items_length, %edi
+    jge endall
+    incl %edi
+    movl data_items(,%edi,4), %eax
+    cmp %ebx, %eax
+    jle loop
+    movl %eax, %ebx
+    jmp loop
+
+endall:
+    movl $1, %eax
+    int $0x80
