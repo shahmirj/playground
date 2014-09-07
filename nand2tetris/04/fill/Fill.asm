bc987f316460802317d77006e7b0d68a6ec91225
Shahmir Javaid <shahmirj@gmail.com>
2014-09-07 15:06:49 +0100

    Got the File.asm working as expected

--- nand2tetris/04/fill/Fill.asm
+++ nand2tetris/04/fill/Fill.asm
@@ -8,4 +8,69 @@
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+
+// Set up our R0 register to point to the first screen pixel
+(INIT)
+
+    // Reset our R0 to point to screen so we can create a counter
+    // To go UP by one
+    @SCREEN
+    D=A
+    @R0
+    M=D // Start our counter at the screen[0] position
+
+// Go through each screen position stored at @R0 and set the value
+// to the color represented by key-down or key-up. Note key-down should
+// draw -1 and key-up should draw 0 
+//
+// In the end increment the screen pointer up one to go to the next
+// Screen value 
+(LOOP)
+
+// Read the value from our input and store it in R2
+(INPUT)
+    @KBD
+    D=M
+    @R2
+    M=D
+
+    // Jump to white if the input is <= 0
+    @WHITE
+    D;JLE
+
+// This bit will change the code to black
+(BLACK)
+    // Set to black, -1 is all 1111111.. in the address
+    D=-1
+
+    // Use this if you want D to have the value of input
+    //@R2 
+    //D=M
+
+    @LOGIC
+    0;JMP
+
+// This bit will change the code to white
+(WHITE)
+    D=0
+
+(LOGIC)
+
+    // Load the R0 address, set our address to the value of R0
+    // And then set the A register to have the value of D
+    @R0
+    A=M
+    M=D
+
+    // add one to R0 so we can draw on the next part of the screen
+    @R0
+    M=M+1
+
+    // Keep going
+    @LOOP
+    0;JMP
+
+// Bye Bye!
+(END)
+    @END
+    0;JMP
