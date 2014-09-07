8cbc23f0a464751fc5f9874d420eeab96b4a22dc
Shahmir Javaid <shahmirj@gmail.com>
2014-09-07 03:11:17 +0100

    Got the Multiplier Working

--- nand2tetris/04/mult/Mult.asm
+++ nand2tetris/04/mult/Mult.asm
@@ -6,4 +6,31 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
-// Put your code here.
+// Initialize R2 to 0 because previous entries may have polluted this field
+@R2
+M=0
+
+(LOOP)
+
+    // Subtract R0 and check if it is less than 0
+    @R0
+    D=M-1
+    M=D
+
+    // If it is less than 0 then end
+    @END
+    D;JLT
+
+    // Load R1 and Add R2+R1
+    @R1
+    D=M
+    @R2
+    M=M+D
+
+    // Go to the start of the loop
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
